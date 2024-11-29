4264f63b17624a26aca7013b9ae8d088e0897917
matesszabo <matesszabo@hotmail.com>
2024-11-29 13:40:00 +0100

    DB works

--- src/main/java/hu/unideb/inf/lev/bootstrap_lev/model/Person.java
+++ src/main/java/hu/unideb/inf/lev/bootstrap_lev/model/Person.java
@@ -3,10 +3,12 @@ package hu.unideb.inf.lev.bootstrap_lev.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "Persons")
 public class Person {
     @Id
     @GeneratedValue
