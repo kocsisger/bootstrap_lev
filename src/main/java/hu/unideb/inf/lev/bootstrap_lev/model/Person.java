e202e3f5ca72188bd38a20038d77f8150d0e9508
matesszabo <matesszabo@hotmail.com>
2024-11-29 13:32:24 +0100

    Person entity added

---
+++ src/main/java/hu/unideb/inf/lev/bootstrap_lev/model/Person.java
@@ -0,0 +1,19 @@
+package hu.unideb.inf.lev.bootstrap_lev.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean enabled;
+}
