cc132e304407faf54d24c82b93a14e0b9d57ce94
matesszabo <matesszabo@hotmail.com>
2024-11-29 14:39:20 +0100

    _C_ R U D

--- src/main/java/hu/unideb/inf/lev/bootstrap_lev/controller/MainController.java
+++ src/main/java/hu/unideb/inf/lev/bootstrap_lev/controller/MainController.java
@@ -1,12 +1,28 @@
 package hu.unideb.inf.lev.bootstrap_lev.controller;
 
+import hu.unideb.inf.lev.bootstrap_lev.model.Person;
+import hu.unideb.inf.lev.bootstrap_lev.model.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
     @GetMapping("")
     public String showHomePage(){
         return "index";
+    }
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @GetMapping("/persons")
+    public String getAllPersons(Model model){
+        List<Person> personsList = personRepository.findAll();
+        model.addAttribute("personsList", personsList);
+        return "persons";
     }
 }
---
+++ src/main/java/hu/unideb/inf/lev/bootstrap_lev/model/PersonRepository.java
@@ -0,0 +1,6 @@
+package hu.unideb.inf.lev.bootstrap_lev.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
