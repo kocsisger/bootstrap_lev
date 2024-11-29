92a4e831a3fef683dc90258b5f31ae22b92d7785
matesszabo <matesszabo@hotmail.com>
2024-11-29 13:50:02 +0100

    Hello world

---
+++ src/main/java/hu/unideb/inf/lev/bootstrap_lev/controller/MainController.java
@@ -0,0 +1,12 @@
+package hu.unideb.inf.lev.bootstrap_lev.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("")
+    public String showHomePage(){
+        return "index";
+    }
+}
