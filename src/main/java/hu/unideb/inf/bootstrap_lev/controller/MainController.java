793f62118673186fd7cf179cc37da6eaf75ff387
Kocsis Gergely <kocsisger@example.com>
2025-12-12 13:10:14 +0100

    CRUD

--- src/main/java/hu/unideb/inf/bootstrap_lev/controller/MainController.java
+++ src/main/java/hu/unideb/inf/bootstrap_lev/controller/MainController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -23,10 +25,30 @@ public class MainController {
         return "persons";
     }
 
+    @GetMapping("/persons/edit/{id}")
+    public String editPerson(Model model, @PathVariable long id) {
+        model.addAttribute("newPerson", personRepository.findById(id).get());
+        model.addAttribute("titleText", "Edit Person");
+        return "newPersonForm";
+    }
+
     @GetMapping("/persons/new")
     public String addNewPerson(Model model) {
         model.addAttribute("newPerson", new Person());
+        model.addAttribute("titleText", "Add New Person");
         return "newPersonForm";
+    }
+
+    @PostMapping("/persons/save")
+    public String savePerson(Person person) {
+        personRepository.save(person);
+        return "redirect:/persons";
+    }
+
+    @GetMapping("/persons/delete/{id}")
+    public String deletePerson(@PathVariable long id) {
+        personRepository.deleteById(id);
+        return "redirect:/persons";
     }
 
 }
