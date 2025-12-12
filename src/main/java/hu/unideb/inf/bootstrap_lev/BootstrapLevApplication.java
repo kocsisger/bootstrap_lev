a65ed3428533fd1b6613ce7e6614904a691a97f5
Kocsis Gergely <kocsisger@example.com>
2025-12-12 10:34:05 +0100

    Initial commit

---
+++ src/main/java/hu/unideb/inf/bootstrap_lev/BootstrapLevApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.bootstrap_lev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapLevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapLevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/unideb/inf/bootstrap_lev/BootstrapLevApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.bootstrap_lev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapLevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
